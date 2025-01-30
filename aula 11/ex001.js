b56261b413e498f8a91ce58ad0297811a6c13e5a
loiles <luisasantos084@gmail.com>
2025-01-30 17:49:34 -0300

    index.js

---
+++ aula 11/ex001.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel>60){
+    console.log(`Voce ultrapassou a velocidade permitida. Multado`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
