61d6640be428dcc70321a68ba24b8b75018c1bfa
loiles <luisasantos084@gmail.com>
2025-01-30 18:10:55 -0300

    index.js

---
+++ aula 11/ex002.js
@@ -0,0 +1,10 @@
+var time = 'Botafogo'
+console.log(`torcendo para ${time}`)
+if (time != ' Botafogo') {
+    console.log('voce é escolhido')
+} else {
+    console.log('voce é rubro negro safado')
+}
+
+    
+
